10387f6442754457aa62d30b981852ed407eee7c
Caballero Urrego <141086114+CaballeroUrrego@users.noreply.github.com>
2024-09-13 07:38:07 -0500

    Configuracion de strings

--- 03_strings.py
+++ 03_strings.py
@@ -1,43 +1,65 @@
-## Strings ##
-my_string="Mi String"
-my_other_sting= 'Mi otro String'
-print (len(my_string))
-print (len(my_other_sting))
+# Clase en vídeo: https://youtu.be/Kp4Mvapo5kc?t=8643
 
-print(my_string + " " +my_other_sting )   
+### Strings ###
 
-my_new_line_string = "Este es un String \n Con salto de linea"
+my_string = "Mi String"
+my_other_string = 'Mi otro String'
 
-print(my_new_line_string )
+print(len(my_string))
+print(len(my_other_string))
+print(my_string + " " + my_other_string)
 
-my_tab_string = "\t Este es un String con tabulacion"
+my_new_line_string = "Este es un String\ncon salto de línea"
+print(my_new_line_string)
 
-print(my_new_line_string )
-print(my_tab_string )
+my_tab_string = "\tEste es un String con tabulación"
+print(my_tab_string)
 
-my_scape_string ="\tEste es un String \nEscapado"
+my_scape_string = "\\tEste es un String \\n escapado"
 print(my_scape_string)
 
-#Formateo
+# Formateo
 
+name, surname, age = "Caballero", "Urrego", 23
+print("Mi nombre es {} {} y mi edad es {}".format(name, surname, age))
+print("Mi nombre es %s %s y mi edad es %d" % (name, surname, age))
+print("Mi nombre es " + name + " " + surname + " y mi edad es " + str(age))
+print(f"Mi nombre es {name} {surname} y mi edad es {age}")
 
-name,surname ,age = "Caballero", "Urrego",23
+# Desempaqueado de caracteres
 
-print("Mi nombre es {} {}y mi edad es {}".format(name,surname ,age))
-print("Mi nombre es %s %s y mi edad es %s"%(name,surname ,age))
-print("Mi nombre es" + name+ "" + surname + "y mi edad es " + str(age))
-print(f"Mi nombre es{name}{surname}y mi edad es {age}") 
-
-#Desempaquetado de caracteres 
-lenguage="python"
-a, b, c, d, e, f =lenguage
+language = "python"
+a, b, c, d, e, f = language
 print(a)
 print(e)
 
-#Division
+# División
 
-lenguage_slice=lenguage[1:3] 
-print(lenguage_slice[0])
+language_slice = language[1:3]
+print(language_slice)
 
-lenguage_slice=lenguage[1:] 
-print(lenguage_slice[0])
+language_slice = language[1:]
+print(language_slice)
+
+language_slice = language[-2]
+print(language_slice)
+
+language_slice = language[0:6:2]
+print(language_slice)
+
+# Reverse
+
+reversed_language = language[::-1]
+print(reversed_language)
+
+# Funciones del lenguaje
+
+print(language.capitalize())
+print(language.upper())
+print(language.count("t"))
+print(language.isnumeric())
+print("1".isnumeric())
+print(language.lower())
+print(language.lower().isupper())
+print(language.startswith("Py"))
+print("Py" == "py")  # No es lo mismo
